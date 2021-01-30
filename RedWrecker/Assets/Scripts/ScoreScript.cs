56c971d2d6df420c971260ae1cdaa0d5b5ada73d
Matthew Parker <maddparker@gmail.com>
2021-01-30 15:09:53 -0600

    Added Slope Friction and Score

---
+++ RedWrecker/Assets/Scripts/ScoreScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreScript : MonoBehaviour
+{
+    Text text;
+    float startTime;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<Text>();
+        startTime = Time.realtimeSinceStartup;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = "Score: " + (Time.realtimeSinceStartup - startTime);
+    }
+}

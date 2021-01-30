5774a225db0bdde16f5276edce6f1f4f3fe35fe9
Matthew Parker <maddparker@gmail.com>
2021-01-29 22:35:15 -0600

    Added Obstacles

---
+++ RedWrecker/Assets/Scripts/GenerateObstacles.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateObstacles : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Here");
+        for (int i = 0; i < 100; i++)
+        {
+            GameObject go = 
+                Instantiate<GameObject>(Resources.Load<GameObject>("Block"));
+            go.transform.position = 
+                new Vector3(
+                    Random.Range(-3.5f, 3.5f),
+                    0.5f,
+                    i * 10 - 500);
+            
+            go.transform.Rotate(Vector3.up, 45);
+            
+            go.transform.parent = transform;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

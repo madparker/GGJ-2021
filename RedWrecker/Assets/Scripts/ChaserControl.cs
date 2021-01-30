86189963dfe17bd9938a577eadd80f04c88d556b
Matthew Parker <maddparker@gmail.com>
2021-01-29 23:07:09 -0600

    Added Chaser, Tweaked physics

---
+++ RedWrecker/Assets/Scripts/ChaserControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChaserControl : MonoBehaviour
+{
+    Rigidbody rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = -transform.forward * 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = -transform.forward * 10.1f;
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+           Destroy(other.gameObject);
+    }
+    
+}

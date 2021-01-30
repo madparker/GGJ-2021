928a25c412daf4183b70d0fd495e36191bb80d73
Matthew Parker <maddparker@gmail.com>
2021-01-29 21:50:32 -0600

    Simple Controller

---
+++ RedWrecker/Assets/Scripts/CarControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarControl : MonoBehaviour
+{
+    public float forceMod = 10;
+    
+    Rigidbody rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(transform.right * forceMod);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(transform.right * -forceMod);
+        }
+    }
+}
